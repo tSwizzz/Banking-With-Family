1a4f21aaa9a3dffb8f10e3bd36af9631e3c2002b
Eddy G <eddy.spambari@gmail.com>
2023-05-13 05:39:09 -0700

    some comments added

--- frontend/script.js
+++ frontend/script.js
@@ -4,17 +4,16 @@ const abi = [
   "event Deposit(address indexed user, uint256 indexed accountId, uint256 value, uint256 timestamp)",
   "event Withdraw(uint256 indexed withdrawId, uint256 timestamp)",
   "event WithdrawRequested(address indexed user, uint256 indexed accountId, uint256 indexed withdrawId, uint256 amount, uint256 timestamp)",
-  "function approveWithdrawl(uint256 accountId, uint256 withdrawId)",
+  "function approveWithdrawal(uint256 accountId, uint256 withdrawId)",
   "function createAccount(address[] otherOwners)",
   "function deposit(uint256 accountId) payable",
   "function getAccounts() view returns (uint256[])",
   "function getApprovals(uint256 accountId, uint256 withdrawId) view returns (uint256)",
   "function getBalance(uint256 accountId) view returns (uint256)",
   "function getOwners(uint256 accountId) view returns (address[])",
-  "function requestWithdrawl(uint256 accountId, uint256 amount)",
+  "function requestWithdrawal(uint256 accountId, uint256 amount)",
   "function withdraw(uint256 accountId, uint256 withdrawId)",
 ];
-
 const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 let contract = null;
 
