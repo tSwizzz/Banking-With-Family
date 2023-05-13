568b531e23408b7f4f96a28592dae75dd78a6d53
Eddy G <eddy.spambari@gmail.com>
2023-05-12 19:35:57 -0700

    finish contract/tests

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -1,34 +1,26 @@
 pragma solidity >=0.4.22 <=0.8.19;
 
 contract BankAccount {
-    event AccountCreated(
-        address[] owners, 
-        uint256 indexed id, 
-        uint256 timestamp
-    );
     event Deposit(
         address indexed user,
         uint256 indexed accountId,
-        uint256 value, 
+        uint256 value,
         uint256 timestamp
     );
     event WithdrawRequested(
         address indexed user,
-        uint256 indexed accountId, 
-        uint256 indexed withdrawId, 
-        uint256 amount, 
+        uint256 indexed accountId,
+        uint256 indexed withdrawId,
+        uint256 amount,
         uint256 timestamp
     );
-    event Withdraw(
-        uint256 indexed withdrawId, 
+    event Withdraw(uint256 indexed withdrawId, uint256 timestamp);
+    event AccountCreated(
+        address[] owners,
+        uint256 indexed id,
         uint256 timestamp
     );
 
-    struct Account {
-        address[] owners;
-        uint256 balance;
-        mapping(uint256 => WithdrawRequest) withdrawRequests;
-    }
     struct WithdrawRequest {
         address user;
         uint256 amount;
@@ -37,16 +29,22 @@ contract BankAccount {
         bool approved;
     }
 
-    mapping(uint256 => Account) accounts; //an accountID that points to Account
-    mapping(address => uint256[]) userAccounts; //list of different accounts user may have
+    struct Account {
+        address[] owners;
+        uint256 balance;
+        mapping(uint256 => WithdrawRequest) withdrawRequests;
+    }
+
+    mapping(uint256 => Account) accounts;
+    mapping(address => uint256[]) userAccounts;
 
     uint256 nextAccountId;
     uint256 nextWithdrawId;
 
     modifier accountOwner(uint256 accountId) {
         bool isOwner;
-        for(uint256 k; k < accounts[accountId].owners.length; k++) {
-            if(msg.sender == accounts[accountId].owners[k]) {
+        for (uint256 idx; idx < accounts[accountId].owners.length; idx++) {
+            if (accounts[accountId].owners[idx] == msg.sender) {
                 isOwner = true;
                 break;
             }
@@ -57,13 +55,14 @@ contract BankAccount {
 
     modifier validOwners(address[] calldata owners) {
         require(owners.length + 1 <= 4, "maximum of 4 owners per account");
-        for(uint256 k; k < owners.length; k++) {
-            if(owners[k] == msg.sender) {
-                revert("no duplicate owners allowed");
+        for (uint256 i; i < owners.length; i++) {
+            if (owners[i] == msg.sender) {
+                revert("no duplicate owners");
             }
-            for(uint256 f = k + 1; f < owners.length; f++) {
-                if(owners[k] == owners[f]) {
-                    revert("no duplicate owners allowed");
+
+            for (uint256 j = i + 1; j < owners.length; j++) {
+                if (owners[i] == owners[j]) {
+                    revert("no duplicate owners");
                 }
             }
         }
@@ -76,45 +75,65 @@ contract BankAccount {
     }
 
     modifier canApprove(uint256 accountId, uint256 withdrawId) {
-        require(!accounts[accountId].withdrawRequests[withdrawId].approved, "this request is already approved");
-        require(accounts[accountId].withdrawRequests[withdrawId].user != msg.sender, "you cannot approve this request");
-        require(accounts[accountId].withdrawRequests[withdrawId].user != address(0), "this request does not exist");
-        require(!accounts[accountId].withdrawRequests[withdrawId].ownersApproved[msg.sender], "you have already approved this request");
+        require(
+            !accounts[accountId].withdrawRequests[withdrawId].approved,
+            "this request is already approved"
+        );
+        require(
+            accounts[accountId].withdrawRequests[withdrawId].user != msg.sender,
+            "you cannot approve this request"
+        );
+        require(
+            accounts[accountId].withdrawRequests[withdrawId].user != address(0),
+            "this request does not exist"
+        );
+        require(
+            !accounts[accountId].withdrawRequests[withdrawId].ownersApproved[
+                msg.sender
+            ],
+            "you have already approved this request"
+        );
         _;
     }
 
-    modifier canWithdraw(uint accountId, uint withdrawId) {
-        require(accounts[accountId].withdrawRequests[withdrawId].user == msg.sender, "you did not create this request");
-        require(accounts[accountId].withdrawRequests[withdrawId].approved, "this request is not approved");
+    modifier canWithdraw(uint256 accountId, uint256 withdrawId) {
+        require(
+            accounts[accountId].withdrawRequests[withdrawId].user == msg.sender,
+            "you did not create this request"
+        );
+        require(
+            accounts[accountId].withdrawRequests[withdrawId].approved,
+            "this request is not approved"
+        );
         _;
     }
 
-    function deposit(uint256 accountId) 
-        external 
-        payable 
-        accountOwner(accountId) 
+    function deposit(uint256 accountId)
+        external
+        payable
+        accountOwner(accountId)
     {
         accounts[accountId].balance += msg.value;
     }
 
-    function createAccount(address[] calldata otherOwners) 
-        external 
-        validOwners(otherOwners) 
+    function createAccount(address[] calldata otherOwners)
+        external
+        validOwners(otherOwners)
     {
         address[] memory owners = new address[](otherOwners.length + 1);
         owners[otherOwners.length] = msg.sender;
 
         uint256 id = nextAccountId;
 
-        for(uint256 k; k < owners.length; k++) {
-            if(k < owners.length - 1) {
-                owners[k] = otherOwners[k];
+        for (uint256 idx; idx < owners.length; idx++) {
+            if (idx < owners.length - 1) {
+                owners[idx] = otherOwners[idx];
             }
 
-            if(userAccounts[owners[k]].length > 2) {
+            if (userAccounts[owners[idx]].length > 2) {
                 revert("each user can have a max of 3 accounts");
             }
-            userAccounts[owners[k]].push(id);
+            userAccounts[owners[idx]].push(id);
         }
 
         accounts[id].owners = owners;
@@ -123,44 +142,52 @@ contract BankAccount {
     }
 
     function requestWithdrawal(uint256 accountId, uint256 amount)
-        external 
+        external
         accountOwner(accountId)
-        sufficientBalance(accountId, amount) 
+        sufficientBalance(accountId, amount)
     {
         uint256 id = nextWithdrawId;
-        WithdrawRequest storage request = accounts[accountId].withdrawRequests[id];
-
-        //updates WithdrawRequest struct with appropriate values and then emits
+        WithdrawRequest storage request = accounts[accountId].withdrawRequests[
+            id
+        ];
         request.user = msg.sender;
         request.amount = amount;
         nextWithdrawId++;
-        emit WithdrawRequested(msg.sender, accountId, id, amount, block.timestamp);
+        emit WithdrawRequested(
+            msg.sender,
+            accountId,
+            id,
+            amount,
+            block.timestamp
+        );
     }
 
-    function approveWithdrawal(uint256 accountId, uint256 withdrawId) 
-        external 
-        accountOwner(accountId) 
+    function approveWithdrawal(uint256 accountId, uint256 withdrawId)
+        external
+        accountOwner(accountId)
         canApprove(accountId, withdrawId)
-        {
-            WithdrawRequest storage request = accounts[accountId].withdrawRequests[withdrawId];
-            request.approvals++;
-            request.ownersApproved[msg.sender] = true;
-
-            if(request.approvals == accounts[accountId].owners.length - 1) {
-                request.approved = true;
-            }
-        }
-
-    function withdraw(uint256 accountId, uint256 withdrawId) 
-        external 
-        canWithdraw(accountId, withdrawId) 
     {
-        uint amount = accounts[accountId].withdrawRequests[withdrawId].amount;
+        WithdrawRequest storage request = accounts[accountId].withdrawRequests[
+            withdrawId
+        ];
+        request.approvals++;
+        request.ownersApproved[msg.sender] = true;
+
+        if (request.approvals == accounts[accountId].owners.length - 1) {
+            request.approved = true;
+        }
+    }
+
+    function withdraw(uint256 accountId, uint256 withdrawId)
+        external
+        canWithdraw(accountId, withdrawId)
+    {
+        uint256 amount = accounts[accountId]
+            .withdrawRequests[withdrawId]
+            .amount;
         require(accounts[accountId].balance >= amount, "insufficient balance");
 
         accounts[accountId].balance -= amount;
-
-        //resets all values in WithdrawRequest struct
         delete accounts[accountId].withdrawRequests[withdrawId];
 
         (bool sent, ) = payable(msg.sender).call{value: amount}("");
@@ -169,35 +196,27 @@ contract BankAccount {
         emit Withdraw(withdrawId, block.timestamp);
     }
 
-    function getBalance(uint256 accountId) 
-        public 
-        view 
-        returns(uint256) 
-    {
+    function getBalance(uint256 accountId) public view returns (uint256) {
         return accounts[accountId].balance;
     }
 
-    function getOwners(uint256 accountId) 
-        public 
-        view 
-        returns(address[] memory) 
+    function getOwners(uint256 accountId)
+        public
+        view
+        returns (address[] memory)
     {
         return accounts[accountId].owners;
     }
 
-    function getApprovals(uint256 accountId, uint256 withdrawId) 
-        public 
-        view 
-        returns(uint256) 
+    function getApprovals(uint256 accountId, uint256 withdrawId)
+        public
+        view
+        returns (uint256)
     {
         return accounts[accountId].withdrawRequests[withdrawId].approvals;
     }
 
-    function getAccounts() 
-        public 
-        view 
-        returns(uint256[] memory) 
-    {
+    function getAccounts() public view returns (uint256[] memory) {
         return userAccounts[msg.sender];
     }
 }
--- test/BankAccount.js
+++ test/BankAccount.js
@@ -12,24 +12,55 @@ describe("BankAccount", function () {
     return { bankAccount, addr0, addr1, addr2, addr3, addr4 };
   }
 
+  async function deployBankAccountWithAccounts(
+    owners = 1,
+    deposit = 0,
+    withdrawalAmounts = []
+  ) {
+    const { bankAccount, addr0, addr1, addr2, addr3, addr4 } =
+      await loadFixture(deployBankAccount);
+    let addresses = [];
+
+    if (owners == 2) addresses = [addr1.address];
+    else if (owners == 3) addresses = [addr1.address, addr2.address];
+    else if (owners == 4)
+      addresses = [addr1.address, addr2.address, addr3.address];
+
+    await bankAccount.connect(addr0).createAccount(addresses);
+
+    if (deposit > 0) {
+      await bankAccount
+        .connect(addr0)
+        .deposit(0, { value: deposit.toString() });
+    }
+
+    for (const withdrawalAmount of withdrawalAmounts) {
+      await bankAccount.connect(addr0).requestWithdrawal(0, withdrawalAmount);
+    }
+
+    return { bankAccount, addr0, addr1, addr2, addr3, addr4 };
+  }
+
   describe("Deployment", () => {
     it("Should deploy without error", async () => {
       await loadFixture(deployBankAccount);
-    })
+    });
   });
 
   describe("Creating an account", () => {
-    it("Should allow creating a single user account", async () => {
-        const { bankAccount, addr0 } = await loadFixture(deployBankAccount);
-        await bankAccount.connect(addr0).createAccount([]);
-        const accounts = await bankAccount.connect(addr0).getAccounts();
-        expect(accounts.length).to.equal(1);
+    it("should allow creating a single user account", async () => {
+      const { bankAccount, addr0 } = await loadFixture(deployBankAccount);
+      await bankAccount.connect(addr0).createAccount([]);
+      const accounts = await bankAccount.connect(addr0).getAccounts();
+      expect(accounts.length).to.equal(1);
     });
 
-    it("Should allow creating a double user account", async () => {
-      const { bankAccount, addr0, addr1 } = await loadFixture(deployBankAccount);
+    it("should allow creating a double user account", async () => {
+      const { bankAccount, addr0, addr1 } = await loadFixture(
+        deployBankAccount
+      );
       await bankAccount.connect(addr0).createAccount([addr1.address]);
-      
+
       const accounts1 = await bankAccount.connect(addr0).getAccounts();
       expect(accounts1.length).to.equal(1);
 
@@ -37,10 +68,14 @@ describe("BankAccount", function () {
       expect(accounts2.length).to.equal(1);
     });
 
-    it("Should allow creating a triple user account", async () => {
-      const { bankAccount, addr0, addr1, addr2 } = await loadFixture(deployBankAccount);
-      await bankAccount.connect(addr0).createAccount([addr1.address, addr2.address]);
-      
+    it("should allow creating a triple user account", async () => {
+      const { bankAccount, addr0, addr1, addr2 } = await loadFixture(
+        deployBankAccount
+      );
+      await bankAccount
+        .connect(addr0)
+        .createAccount([addr1.address, addr2.address]);
+
       const accounts1 = await bankAccount.connect(addr0).getAccounts();
       expect(accounts1.length).to.equal(1);
 
@@ -51,10 +86,14 @@ describe("BankAccount", function () {
       expect(accounts3.length).to.equal(1);
     });
 
-    it("Should allow creating a quad user account", async () => {
-      const { bankAccount, addr0, addr1, addr2, addr3 } = await loadFixture(deployBankAccount);
-      await bankAccount.connect(addr0).createAccount([addr1.address, addr2.address, addr3.address]);
-      
+    it("should allow creating a quad user account", async () => {
+      const { bankAccount, addr0, addr1, addr2, addr3 } = await loadFixture(
+        deployBankAccount
+      );
+      await bankAccount
+        .connect(addr0)
+        .createAccount([addr1.address, addr2.address, addr3.address]);
+
       const accounts1 = await bankAccount.connect(addr0).getAccounts();
       expect(accounts1.length).to.equal(1);
 
@@ -68,34 +107,173 @@ describe("BankAccount", function () {
       expect(accounts4.length).to.equal(1);
     });
 
-    it("Should not allow creating an account with duplicate owners", async () => {
+    it("should not allow creating an account with duplicate owners", async () => {
       const { bankAccount, addr0 } = await loadFixture(deployBankAccount);
-      await expect(bankAccount.connect(addr0).createAccount([addr0.address])).to.be.reverted;
+      await expect(bankAccount.connect(addr0).createAccount([addr0.address])).to
+        .be.reverted;
     });
 
-    it("Should not allow creating an account with 5 owners", async () => {
-      const { bankAccount, addr0, addr1, addr2, addr3, addr4 } = await loadFixture(deployBankAccount);
+    it("should not allow creating an account with 5 owners", async () => {
+      const { bankAccount, addr0, addr1, addr2, addr3, addr4 } =
+        await loadFixture(deployBankAccount);
       await expect(
         bankAccount
           .connect(addr0)
           .createAccount([
-            addr0.address, 
-            addr1.address, 
-            addr2.address, 
-            addr3.address, 
-            addr4.address
+            addr0.address,
+            addr1.address,
+            addr2.address,
+            addr3.address,
+            addr4.address,
           ])
-        ).to.be.reverted;
+      ).to.be.reverted;
     });
 
-    it("Should not allow creating an account with 5 owners", async () => {
+    it("should not allow creating an account with 5 owners", async () => {
       const { bankAccount, addr0 } = await loadFixture(deployBankAccount);
 
-      for(let k = 0; k < 3; k++) {
+      for (let idx = 0; idx < 3; idx++) {
         await bankAccount.connect(addr0).createAccount([]);
       }
 
       await expect(bankAccount.connect(addr0).createAccount([])).to.be.reverted;
+    });
+  });
+
+  describe("Despositing", () => {
+    it("should allow deposit from account owner", async () => {
+      const { bankAccount, addr0 } = await deployBankAccountWithAccounts(1);
+      await expect(
+        bankAccount.connect(addr0).deposit(0, { value: "100" })
+      ).to.changeEtherBalances([bankAccount, addr0], ["100", "-100"]);
+    });
+
+    it("should NOT allow deposit from non-account owner", async () => {
+      const { bankAccount, addr1 } = await deployBankAccountWithAccounts(1);
+      await expect(bankAccount.connect(addr1).deposit(0, { value: "100" })).to
+        .be.reverted;
+    });
+  });
+
+  describe("Withdraw", () => {
+    describe("Request a withdraw", () => {
+      it("account owner can request withdraw", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts(
+          1,
+          100
+        );
+        await bankAccount.connect(addr0).requestWithdrawal(0, 100);
+      });
+
+      it("account owner can not request withdraw with invalid amount", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts(
+          1,
+          100
+        );
+        await expect(bankAccount.connect(addr0).requestWithdrawal(0, 101)).to.be
+          .reverted;
+      });
+
+      it("non-account owner cannot request withdraw", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts(
+          1,
+          100
+        );
+        await expect(bankAccount.connect(addr1).requestWithdrawal(0, 90)).to.be
+          .reverted;
+      });
+
+      it("non-account owner cannot request withdraw", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts(
+          1,
+          100
+        );
+        await bankAccount.connect(addr0).requestWithdrawal(0, 90);
+        await bankAccount.connect(addr0).requestWithdrawal(0, 10);
+      });
+    });
+
+    describe("Approve a withdraw", () => {
+      it("should allow account owner to approve withdraw", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        await bankAccount.connect(addr1).approveWithdrawal(0, 0);
+        expect(await bankAccount.getApprovals(0, 0)).to.equal(1);
+      });
+
+      it("should not allow non-account owner to approve withdraw", async () => {
+        const { bankAccount, addr2 } = await deployBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        await expect(bankAccount.connect(addr2).approveWithdrawal(0, 0)).to.be
+          .reverted;
+      });
+
+      it("should not allow owner to approve withdrawal multiple times", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        bankAccount.connect(addr1).approveWithdrawal(0, 0);
+        await expect(bankAccount.connect(addr1).approveWithdrawal(0, 0)).to.be
+          .reverted;
+      });
+
+      it("should not allow creator of request to approve request", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts(
+          2,
+          100,
+          [100]
+        );
+        await expect(bankAccount.connect(addr0).approveWithdrawal(0, 0)).to.be
+          .reverted;
+      });
+    });
+
+    describe("Make withdraw", () => {
+      it("should allow creator of request to withdraw approved request", async () => {
+        const { bankAccount, addr0, addr1 } =
+          await deployBankAccountWithAccounts(2, 100, [100]);
+        await bankAccount.connect(addr1).approveWithdrawal(0, 0);
+        await expect(
+          bankAccount.connect(addr0).withdraw(0, 0)
+        ).to.changeEtherBalances([bankAccount, addr0], ["-100", "100"]);
+      });
+
+      it("should not allow creator of request to withdraw approved request twice", async () => {
+        const { bankAccount, addr0, addr1 } =
+          await deployBankAccountWithAccounts(2, 200, [100]);
+        await bankAccount.connect(addr1).approveWithdrawal(0, 0);
+        await expect(
+          bankAccount.connect(addr0).withdraw(0, 0)
+        ).to.changeEtherBalances([bankAccount, addr0], ["-100", "100"]);
+        await expect(bankAccount.connect(addr0).withdraw(0, 0)).to.be.reverted;
+      });
+
+      it("should not allow non-creator of request to withdraw approved request ", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts(
+          2,
+          200,
+          [100]
+        );
+        await bankAccount.connect(addr1).approveWithdrawal(0, 0);
+        await expect(bankAccount.connect(addr1).withdraw(0, 0)).to.be.reverted;
+      });
+
+      it("should not allow non-creator of request to withdraw approved request ", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts(
+          2,
+          200,
+          [100]
+        );
+        await expect(bankAccount.connect(addr0).withdraw(0, 0)).to.be.reverted;
+      });
     });
   });
 });
